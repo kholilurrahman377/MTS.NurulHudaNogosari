bb88c8c5f9aacba64304d77552baaf9f011ebe1b
kholilurrahman377 <nkholilurrahman@gmail.com>
2025-02-15 13:33:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    document.querySelector(".menu").classList.toggle("active");
+}
+
+function toggleDropdown() {
+    document.querySelector(".dropdown-menu").classList.toggle("active");
+}
